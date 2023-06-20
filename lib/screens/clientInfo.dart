8d8f56bad06a5a4dccd62808bdd1f40f3a5a9980
yosi <yosihofman21@gmail.com>
2023-06-21 00:11:04 +0300

    link phone and mail

--- lib/screens/clientInfo.dart
+++ lib/screens/clientInfo.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'package:url_launcher/url_launcher.dart';
+
 import '../objects/clients.dart';
 
 class clientInfo extends StatelessWidget {
@@ -48,29 +50,99 @@ final Todo user;
               Card(
                 margin: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
                 color: Colors.grey,
-                child: ListTile(
-                  leading: Icon(Icons.phone),
-                  title: Text('${user.phone}'),
+                
+                child: GestureDetector(
+                      onTap: () {
+                final phoneNumber = '+972543462331'; // Replace with the actual phone number
+                _launchPhoneDialer(phoneNumber);
+              },
+                  child: ListTile(
+                    
+                    leading: Icon(Icons.phone),
+                    title: Text('${user.phone}'),
+                  ),
                 ),
               ),
-              Card(
-                margin: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-                color: Colors.grey,
-                child: ListTile(
-                  leading: Icon(Icons.mail),
-                  title: Text('${user.email}'),
+              GestureDetector(
+                  onTap: () {
+    sendEmail('recipient@example.com');
+  },
+                child: Card(
+                  margin: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+                  color: Colors.grey,
+                  child: ListTile(
+                    leading: Icon(Icons.mail),
+                    title: Text('${user.email}'),
+                  ),
+                  
                 ),
               ),
-              Card(
-                margin: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-                color: Colors.grey,
-                child: ListTile(
-                  leading: Icon(Icons.maps_home_work),
-                  title: Text('${user.address}'),
+              InkWell(
+                  onTap: () {
+    openGoogleMaps('latitude,longitude');
+  },
+                child: Card(
+                  margin: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+                  color: Colors.grey,
+                  child: ListTile(
+                    leading: Icon(Icons.maps_home_work),
+                    title: Text('${user.address}'),
+                  ),
                 ),
               ),
             ],
           ),
         ));
+  }
+}
+// link to phone call
+void _launchPhoneDialer(String phoneNumber) async {
+  final url = 'tel:$phoneNumber';
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
+  }
+}
+// link to phone gmail
+void sendEmail(String emailAddress) async {
+  final Uri emailLaunchUri = Uri(
+    scheme: 'mailto',
+    path: emailAddress,
+  );
+  
+  if (await canLaunch(emailLaunchUri.toString())) {
+    await launch(emailLaunchUri.toString());
+  } else {
+    throw 'Could not launch email';
+  }
+}
+// link to phone waze
+void openWaze(String address) async {
+  final Uri wazeUri = Uri(
+    scheme: 'waze',
+    path: '/ul',
+    queryParameters: {'ll': address},
+  );
+
+  if (await canLaunch(wazeUri.toString())) {
+    await launch(wazeUri.toString());
+  } else {
+    throw 'Could not launch Waze';
+  }
+}
+// link to phone maps
+void openGoogleMaps(String address) async {
+  final Uri mapsUri = Uri(
+    scheme: 'https',
+    host: 'www.google.com',
+    path: '/maps/search/',
+    queryParameters: {'api': '1', 'query': address},
+  );
+
+  if (await canLaunch(mapsUri.toString())) {
+    await launch(mapsUri.toString());
+  } else {
+    throw 'Could not launch Google Maps';
   }
 }
