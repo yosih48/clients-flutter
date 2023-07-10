be4453d31aa0e35a80b028653eb6853e1d067ed3
yosi <yosihofman21@gmail.com>
2023-07-10 16:52:34 +0300

    token

---
+++ lib/componenets/auth.dart
@@ -0,0 +1,19 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+// Function to save the authentication token
+void saveAuthToken(String token) async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  await prefs.setString('authToken', token);
+}
+
+// Function to retrieve the authentication token
+Future<String?> getAuthToken() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  return prefs.getString('authToken');
+}
+
+// Function to check if the user is authenticated
+bool isUserAuthenticated() {
+  Future<String?> authToken = getAuthToken();
+  return authToken != null;
+}
